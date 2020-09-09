cf31a71c9083c3021f0b4758cf0fe507d35ebda2
hugo matos <hugocfmatos@gmail.com>
2020-09-09 18:56:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if string==="I love you, Grandma." {
     return "I love you too."
-  } else if (string.toUpperCase===string) {
+  }
+  if (string.toUpperCase===string) {
     return "YES INDEED"
-  } else if (string.toLowerCase===string) {
+  }
+  if (string.toLowerCase===string) {
     return "I can't hear you!"
   }
 
